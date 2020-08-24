9d0d416abdbb72d36b846f91b1656c8330da9b53
xieangxuan <767950460@qq.com>
2020-08-24 13:56:56 +0800

    加入databing工具类

---
+++ bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugDataBindingNumberUtils.java
@@ -0,0 +1,53 @@
+package com.rulerbug.bugutils.Utils;
+
+import android.util.Log;
+
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.InverseMethod;
+
+public class BugDataBindingNumberUtils {
+    @InverseMethod("strToInt")
+    public static String intToStr(int num) {
+        return num + "";
+    }
+
+    public static int strToInt(String num) {
+        return Integer.valueOf(num);
+    }
+
+    @InverseMethod("strToShort")
+    public static String shortToStr(short num) {
+        return num + "";
+    }
+
+    public static short strToShort(String num) {
+        return Short.valueOf(num);
+    }
+
+    @InverseMethod("strToDouble")
+    public static String doubleToStr(double num) {
+        return num + "";
+    }
+
+    public static double strToDouble(String num) {
+        return Double.valueOf(num);
+    }
+
+    @InverseMethod("strToFloat")
+    public static String floatToStr(float num) {
+        return num + "";
+    }
+
+    public static double strToFloat(String num) {
+        return Float.valueOf(num);
+    }
+
+    @InverseMethod("strToLong")
+    public static String longToStr(long num) {
+        return num + "";
+    }
+
+    public static long strToLong(String num) {
+        return Long.valueOf(num);
+    }
+}
