3019fbf4d929f0a1b96bb5d63728e7da2926ae96
xieangxuan <767950460@qq.com>
2020-08-24 14:35:03 +0800

    bug

--- bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugDataBindingNumberUtils.java
+++ bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugDataBindingNumberUtils.java
@@ -4,47 +4,47 @@ import androidx.databinding.InverseMethod;
 
 public class BugDataBindingNumberUtils {
     @InverseMethod("strToInt")
-    public static String intToStr(int num) {
+    public static String intToStr(Integer num) {
         return num + "";
     }
 
-    public static int strToInt(String num) {
+    public static Integer strToInt(String num) {
         return Integer.valueOf(num);
     }
 
     @InverseMethod("strToShort")
-    public static String shortToStr(short num) {
+    public static String shortToStr(Short num) {
         return num + "";
     }
 
-    public static short strToShort(String num) {
+    public static Short strToShort(String num) {
         return Short.valueOf(num);
     }
 
     @InverseMethod("strToDouble")
-    public static String doubleToStr(double num) {
+    public static String doubleToStr(Double num) {
         return num + "";
     }
 
-    public static double strToDouble(String num) {
+    public static Double strToDouble(String num) {
         return Double.valueOf(num);
     }
 
     @InverseMethod("strToFloat")
-    public static String floatToStr(float num) {
+    public static String floatToStr(Float num) {
         return num + "";
     }
 
-    public static float strToFloat(String num) {
+    public static Float strToFloat(String num) {
         return Float.valueOf(num);
     }
 
     @InverseMethod("strToLong")
-    public static String longToStr(long num) {
+    public static String longToStr(Long num) {
         return num + "";
     }
 
-    public static long strToLong(String num) {
+    public static Long strToLong(String num) {
         return Long.valueOf(num);
     }
 }
