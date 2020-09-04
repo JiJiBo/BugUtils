7b3465f30d74bd99b49e545010c55e62a5bda51d
xieangxuan <767950460@qq.com>
2020-09-04 14:33:23 +0800

    fix bug with save Bitmap

--- bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugVersionUtils.java
+++ bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugVersionUtils.java
@@ -21,6 +21,6 @@ public class BugVersionUtils {
     }
 
     public static String getAppVersion() {
-        return getAppVersionName() + "," + getAppVersionCode();
+        return getAppVersionName() + "." + getAppVersionCode();
     }
 }
