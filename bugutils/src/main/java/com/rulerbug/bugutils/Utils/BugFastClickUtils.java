3d4d0b8a5585c6152cebecb9ec8aea24a7766ecb
xieangxuan <767950460@qq.com>
2020-10-15 11:40:32 +0800

    加入快速点击工具类

--- bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugFastClickUtils.java
+++ bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugFastClickUtils.java
@@ -1,26 +1,18 @@
 package com.rulerbug.bugutils.Utils;
 
 public class BugFastClickUtils {
-    private static final int MIN_CLICK_DELAY_TIME = 3000;
     private static long lastClickTime;
 
-    public static boolean isFastClick() {
-        boolean flag = false;
-        long curClickTime = System.currentTimeMillis();
-        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
-            flag = true;
-        }
-        lastClickTime = curClickTime;
-        return flag;
+    public synchronized static boolean isFastClick() {
+        return isFastClick(500);
     }
-
-    public static boolean isFastClick(int time) {
-        boolean flag = false;
-        long curClickTime = System.currentTimeMillis();
-        if ((curClickTime - lastClickTime) >= time) {
-            flag = true;
+    public synchronized static boolean isFastClick(int wait) {
+        long time = System.currentTimeMillis();
+        long range = time - lastClickTime;
+        if (range < wait && range > 0) {
+            return true;
         }
-        lastClickTime = curClickTime;
-        return flag;
+        lastClickTime = time;
+        return false;
     }
 }
