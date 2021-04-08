6f26fc24535e6764299aaea6d902aad7cb02812a
倪震宇 <2597123782@qq.com>
2021-04-08 10:23:47 +0800

    Money

--- bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugContactUtils.java
+++ bugutils/src/main/java/com/rulerbug/bugutils/Utils/BugContactUtils.java
@@ -1,9 +1,17 @@
 package com.rulerbug.bugutils.Utils;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.OperationApplicationException;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 公司：江苏再出发网络科技有限公司
@@ -12,9 +20,9 @@ import java.util.ArrayList;
  * 功能描述：
  */
 public class BugContactUtils {
-//        <uses-permission android:name="android.permission.READ_CONTACTS" />
+    //        <uses-permission android:name="android.permission.READ_CONTACTS" />
 //    <uses-permission android:name="android.permission.WRITE_CONTACTS" />
-    public static ArrayList<MyContacts> getAllContacts( ) {
+    public static ArrayList<MyContacts> getAllContacts() {
         ArrayList<MyContacts> contacts = new ArrayList<MyContacts>();
 
         Cursor cursor = BugApp.getContext().getContentResolver().query(
@@ -66,6 +74,88 @@ public class BugContactUtils {
         public String name;
         public String phone;
         public String note;
+    }
+
+    public static void addContact(String name, String phoneNumber) {
+        // 创建一个空的ContentValues
+        ContentValues values = new ContentValues();
+
+        // 向RawContacts.CONTENT_URI空值插入，
+        // 先获取Android系统返回的rawContactId
+        // 后面要基于此id插入值
+        Uri rawContactUri = BugApp.getContext().getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
+        long rawContactId = ContentUris.parseId(rawContactUri);
+        values.clear();
+
+        values.put(ContactsContract.Contacts.Data.RAW_CONTACT_ID, rawContactId);
+        // 内容类型
+        values.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+        // 联系人名字
+        values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, name);
+        // 向联系人URI添加联系人名字
+        BugApp.getContext().getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+        values.clear();
+
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
+        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        // 联系人的电话号码
+        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneNumber);
+        // 电话类型
+        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
+        // 向联系人电话号码URI添加电话号码
+        BugApp.getContext().getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+        values.clear();
+    }
+
+    public static boolean batchAddContact(List<MyContacts> list) {
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+        int rawContactInsertIndex;
+        if (list == null || list.size() == 0) {
+            return false;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            rawContactInsertIndex = ops.size();
+
+            ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                    .withYieldAllowed(true)
+                    .build());
+            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                            rawContactInsertIndex)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, list.get(i).name)
+                    .withYieldAllowed(true)
+                    .build());
+            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, list.get(i).phone)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                    .withYieldAllowed(true)
+                    .build());
+
+        }
+        try {
+            //这里才调用的批量添加
+            BugApp.getContext().getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            return false;
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static void delByName(String[] names) {
+        BugApp.getContext().getContentResolver().delete(ContactsContract.Data.CONTENT_URI, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + "=?", names);
+
     }
 }
 
