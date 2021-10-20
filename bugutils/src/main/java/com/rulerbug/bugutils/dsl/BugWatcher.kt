7669160bb61bb3b746da4fad417d2034feff7f44
Nas <1270071138@qq.com>
2021-10-20 16:18:42 +0800

    加入textwatcher dsl

---
+++ bugutils/src/main/java/com/rulerbug/bugutils/dsl/BugWatcher.kt
@@ -0,0 +1,42 @@
+package com.rulerbug.bugutils.dsl
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+private typealias after = (edit: Editable?) -> Unit
+private typealias before = (chars: CharSequence?, start: Int, count: Int, after: Int) -> Unit
+private typealias on = (chars: CharSequence?, start: Int, before: Int, count: Int) -> Unit
+
+class BugWatcher : TextWatcher {
+    private var after: after? = null
+    private var before: before? = null
+    private var on: on? = null
+
+    fun after(after: after) {
+        this.after = after
+    }
+
+    fun before(before: before) {
+        this.before = before
+    }
+
+    fun on(on: on) {
+        this.on = on
+    }
+
+    override fun beforeTextChanged(chars: CharSequence?, start: Int, count: Int, after: Int) {
+        return before?.invoke(chars, start, count, after) ?: Unit
+    }
+
+    override fun onTextChanged(chars: CharSequence?, start: Int, before: Int, count: Int) {
+        return on?.invoke(chars, start, before, count) ?: Unit
+    }
+
+    override fun afterTextChanged(edit: Editable?) {
+        return after?.invoke(edit) ?: Unit
+    }
+
+}
+
+fun bugWatcher(watcher: BugWatcher.() -> Unit) = BugWatcher().also(watcher)
