30fe65c2cd77526bdcad899589dde7c349406b80
Nas <1270071138@qq.com>
2021-10-20 16:19:08 +0800

    加入textwatcher dsl

--- bugutils/src/main/java/com/rulerbug/bugutils/dsl/BugWatcher.kt
+++ bugutils/src/main/java/com/rulerbug/bugutils/dsl/BugWatcher.kt
@@ -2,7 +2,6 @@ package com.rulerbug.bugutils.dsl
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
 
 private typealias after = (edit: Editable?) -> Unit
 private typealias before = (chars: CharSequence?, start: Int, count: Int, after: Int) -> Unit
